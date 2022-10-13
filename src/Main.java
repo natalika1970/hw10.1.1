406475cd20b95d2373bf822ae7f749576e03a631
natalika1970 <natalika_70@mail.ru>
2022-10-14 01:07:06 +0300

    выполнила hw10.1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import object.Author;
+import object.Book;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author levTolstoy = new Author("Лев", "Толстой");
+        Author archibaldKronin = new Author("Арчибальд", "Кронин");
+        Book annaKarenina = new Book("Анна Каренина", "Лев Толстой", 1984);
+        Book citadel = new Book("Цитадель", "Арчибальд Кронин", 1968);
+
+
+
+        System.out.println("Книга: annaKarenina.getTitle()" + "annaKarenina.getAuthor().getName " + "annaKarenina.getAuthor().getSurname " + "Год публикации " + annaKarenina.getPublicationYear() );
     }
 }
